68ff8d9691c0a979a10f240a1f88b49ab733af96
Ali Almuqahwi <ali.almuqahwi@gmail.com>
2019-04-02 19:38:42 +0300

    Adding IServiceCollection extension class.

---
+++ src/AreaStartup/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AreaStartup
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAreaStartup<TStartup>(this IServiceCollection services)
+        {
+            //getting all classes that implement IAreaStartup interface.
+            var types = typeof(TStartup)
+                .Assembly
+                .GetTypes()
+                .Where(e => typeof(IAreaStartup).IsAssignableFrom(e))
+                .ToList();
+
+            //register all types into the container.
+            foreach (var type in types)
+            {
+                if((type != null) && (type != typeof(IAreaStartup)))
+                {
+                    services.AddSingleton(typeof(IAreaStartup), type);
+                }
+            }
+
+            var serviceProvider = services.BuildServiceProvider();
+            var startups = serviceProvider.GetServices<IAreaStartup>();
+            foreach (var startup in startups)
+            {
+                startup.ConfigureServices(services);
+            }
+
+            return services;
+        }
+    }
+}
