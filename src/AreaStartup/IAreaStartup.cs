45a8fa79bab3424b7fca2927f3fd8f7d9353119d
Ali Almuqahwi <ali.almuqahwi@gmail.com>
2019-04-02 18:50:39 +0300

    Adding IAreaStartup interface

---
+++ src/AreaStartup/IAreaStartup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AreaStartup
+{
+    public interface IAreaStartup
+    {
+        void ConfigureServices(IServiceCollection services);
+        void Configure(IApplicationBuilder app, IHostingEnvironment env);
+    }
+}
