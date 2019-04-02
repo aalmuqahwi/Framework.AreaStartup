6259e7980b235f19154987667dda26364e739457
Ali Almuqahwi <ali.almuqahwi@gmail.com>
2019-04-02 19:49:15 +0300

    Adding IApplicationBuilder extension class. Closes #2

---
+++ src/AreaStartup/ApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaStartup
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseAreaStartup(this IApplicationBuilder app)
+        {
+            var applicationServices = app.ApplicationServices;
+
+            //getting all registered IAreaStartup.
+            var startups = applicationServices.GetServices<IAreaStartup>();
+            var env = applicationServices.GetService<IHostingEnvironment>();
+
+            foreach (var startup in startups)
+            {
+                startup.Configure(app, env);
+            }
+
+            return app;
+        }
+    }
+}
