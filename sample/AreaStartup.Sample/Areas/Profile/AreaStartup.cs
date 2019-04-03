07b1a012a955b95ac519af0e0e057a8e39093540
Ali Almuqahwi <ali.almuqahwi@gmail.com>
2019-04-03 20:02:07 +0300

    Adding sample project.

---
+++ sample/AreaStartup.Sample/Areas/Profile/AreaStartup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AreaStartup;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AreaStartup.Sample.Areas.Profile
+{
+    public class AreaStartup : IAreaStartup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //add any dependencies to this area.
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
