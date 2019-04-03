36c062c20e9b141bb8d6e9abdda7b7fc73f36a0d
Ali Almuqahwi <ali.almuqahwi@gmail.com>
2019-04-03 20:01:02 +0300

    Updating the version for the packages.

--- src/AreaStartup/ServiceCollectionExtensions.cs
+++ src/AreaStartup/ServiceCollectionExtensions.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AreaStartup
 {
